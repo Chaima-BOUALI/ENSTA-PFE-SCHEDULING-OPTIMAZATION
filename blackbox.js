0acf5321d496c7358bc2ff769007c350aa8752a6
Chayma Boualy <chayma.boualy@esprit.tn>
2023-02-06 16:55:12 +0100

    Autogenerating GANTT Diag using Pragmadev Autogenerated CSV

--- blackbox.js
+++ blackbox.js
@@ -1,4 +1,19 @@
-// scheduling code in python?                                  
+// string to float in python?   
+var values = "554,20".split(",")
+var v1 = parseFloat(values[0])
+var v2 = parseFloat(values[1])
+
+
+var value = parseFloat("554,20".replace(",", "."));
+
+
+
+
+//Source: https://stackoverflow.com/questions/642650
+
+
+
+
 
 
 
